a4e9b6f2025561e415a1793a7d569e6e3884b784
steve <stevesiu1013@gmail.com>
2026-02-24 19:00:22 +1100

    fix

--- steve/run_flash_attn_turing.py
+++ steve/run_flash_attn_turing.py
@@ -2,6 +2,7 @@ import modal
 from modal import App, Image
 import subprocess
 import sys
+from pathlib import Path
 
 cuda_version = "12.4.0"  # should be no greater than host CUDA version
 flavor = "devel"  #  includes full CUDA toolkit
@@ -15,7 +16,10 @@ image = (
         "torch",
         "setuptools",
         "ninja",
-        "wheel"
+        "wheel",
+        "pytest",
+        "pandas",
+        "openpyxl"
     )
 )
 
@@ -27,55 +31,32 @@ def execute_command(command: str):
 
 app = App(image=image)
 
+VOLUME_NAME = "flash-attn-outputs"
+outputs = modal.Volume.from_name(VOLUME_NAME, create_if_missing=True)
+OUTPUTS_PATH = Path("/outputs")
+
 #@app.function(gpu="T4", container_idle_timeout=60, cpu=16.0, memory=65536)
-@app.function(gpu="T4", cpu=16.0, memory=65536)
+@app.function(gpu="T4", cpu=8.0, memory=32768, volumes={OUTPUTS_PATH: outputs}, timeout=6000)
 def run_extension():
-    # # flash attention dev
-    # import os
-    # execute_command("git clone https://github.com/ssiu/flash-attention-turing.git")
-    # os.chdir("flash-attention-turing")
-    # execute_command("git checkout dev")
-    # execute_command("ls")
-    # #execute_command("git submodule update --init --recursive")
-    # execute_command("git clone https://github.com/NVIDIA/cutlass.git")
-    # os.environ["CXX"] = "g++"
-    # os.environ["CC"] = "gcc"
-    # # execute_command("ls /root/flash-attention-turing/csrc/")
-    # # execute_command("ls /root/flash-attention-turing/csrc/cutlass/")
-    # #execute_command("ls /root/flash-attention-turing/csrc/cutlass/include/cute/tensor.hpp")
-    # execute_command("pip install .")
-    # execute_command("python test_flash.py 4 4096 32 128")
 
     # flash attention develop
     import os
     import torch
     execute_command("git clone https://github.com/ssiu/flash-attention-turing.git")
     os.chdir("flash-attention-turing")
-
-    #execute_command("git checkout params")
-    execute_command("git checkout test_params")
+    execute_command("git checkout gqa")
     #execute_command("git submodule update --init --recursive")
     #execute_command("git clone https://github.com/NVIDIA/cutlass.git")
     os.environ["CXX"] = "g++"
     os.environ["CC"] = "gcc"
+    # os.environ["MAX_JOBS"] = "16"
     # execute_command("ls /root/flash-attention-turing/csrc/")
     # execute_command("ls /root/flash-attention-turing/csrc/cutlass/")
     #execute_command("ls /root/flash-attention-turing/csrc/cutlass/include/cute/tensor.hpp")
     execute_command("pip install -v .")
     print(torch.version.cuda)
     print(torch.__version__)
-    #execute_command("python utils/test_flash_fwd_causal.py 1 128 1 128 1")
-    #execute_command("python utils/test_flash_fwd_causal.py 1 256 1 128 1")
-    # execute_command("python utils/test_flash_fwd_causal.py 1 128 1 128 0")
 
-    # execute_command("python utils/test_flash_fwd_causal.py 4 4096 32 128 0")
-    # execute_command("python utils/test_flash_fwd_causal.py 4 4096 32 128 1")
-
-    # execute_command("python utils/scratch_debug.py")
-
-    # headdim = 64
-    # execute_command("python utils/test_flash_fwd_causal.py 4 4096 32 64 0")
-    # execute_command("python utils/test_flash_fwd_causal.py 4 4096 32 64 1")
-
-    execute_command("python utils/test_flash_fwd_causal.py 4 4096 32 128 0")
-    #execute_command("python utils/test_flash_fwd_causal.py 4 4096 32 128 1")
+    # execute_command("pytest -s -vv test_flash_attn.py::test_flash_attn_bwd")
+    execute_command("pytest -s -vv --tb=short -rfE test_flash_attn.py::test_flash_attn_bwd")
+    outputs.commit()
